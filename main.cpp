57f1317080eeda8264ea49afd8d16c228f8551bd
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-06-13 23:08:10 +0300

    Optimization update

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include <ctime>
 #define PI 3.141592654
 
-const int WIDTH = 400, HEIGHT = 400;
+int WIDTH = 400, HEIGHT = 400;
 
 int main( int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("SDL Clock",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window = SDL_CreateWindow("SDL Clock", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
     if (window == NULL)
     {
@@ -17,18 +17,19 @@ int main( int argc, char *argv[])
         return 1;
     }
 
-    SDL_Texture *texture;
-
+    //Clocksize used for dynamic sizing.
     int ClockSize = (HEIGHT - 100) / 2;
     if(WIDTH < HEIGHT){
         ClockSize = (WIDTH - 100) / 2;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
+    
     const int ClockBoardCorners = 32;
+    int LastSecond = 0;
 
     SDL_Event windowEvent;
+
     while (true)
     {
         if (SDL_PollEvent(&windowEvent))
@@ -38,12 +39,20 @@ int main( int argc, char *argv[])
                 break;
             }
         }
-
-        // Calculates time
+        
+        // -- Calculating the time -- //
         const time_t now = time(nullptr);
         const tm calendar_time = *localtime(std::addressof(now));
-        
         float Seconds = (float)calendar_time.tm_sec;
+        
+        //Skips rendering if data doesn't change.
+        if(Seconds == LastSecond){
+            continue;
+        }
+        else{
+            LastSecond = Seconds;
+        }
+
         float Minutes = (float)calendar_time.tm_min + Seconds / 60.0; // Adds division of the lower unit, so arrays work like in a real analog clock
         float Hours = fmod(((float)calendar_time.tm_hour + Minutes / 60.0), 12.0);
 
@@ -52,13 +61,13 @@ int main( int argc, char *argv[])
         //SDL_RenderPresent(renderer);
         SDL_RenderClear(renderer);
 
+        // -- Clock board -- //
         for (int i = 0; i < ClockBoardCorners; i++)
         {
-            SDL_Vertex vertex_1 = {{WIDTH / 2.0, HEIGHT / 2.0}, {255, 255, 255, 255}, {1, 1}};
+            SDL_Vertex vertex_1 = {{(float)(WIDTH / 2.0), (float)(HEIGHT / 2.0)}, {255, 255, 255, 255}, {1, 1}};
             SDL_Vertex vertex_2 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * 1.05 * ClockSize), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * 1.05 * ClockSize)}, {255, 255, 255, 255}, {1, 1}};
             SDL_Vertex vertex_3 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * 1.05 * ClockSize), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * 1.05 * ClockSize)}, {255, 255, 255, 255}, {1, 1}};
 
-            // Put them into array
             SDL_Vertex vertices[] = {
                 vertex_1,
                 vertex_2,
@@ -68,7 +77,7 @@ int main( int argc, char *argv[])
             SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
         }
 
-        //Marks
+        // -- Marks -- //
         for (int i = 0; i < 60; i ++)
         {
             float angle = fmod(PI * 2.0 / 60 * i, PI * 2.0);
@@ -81,7 +90,7 @@ int main( int argc, char *argv[])
             SDL_RenderDrawLine(renderer, WIDTH / 2.0 + cos(angle) * 0.95 * ClockSize, HEIGHT / 2.0 + sin(angle) * 0.95 * ClockSize, WIDTH / 2.0 + cos(angle) * ClockSize, HEIGHT / 2.0 + sin(angle) * ClockSize);
         }
 
-        // Arrows
+        // -- Arrows -- //
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.0 + cos(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * 0.6 * ClockSize, HEIGHT / 2.0 + sin(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * 0.6 * ClockSize);
 
@@ -90,11 +99,10 @@ int main( int argc, char *argv[])
         SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.0 + cos(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.90 * ClockSize, HEIGHT / 2.0 + sin(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.90 * ClockSize);
         for (int i = 0; i < ClockBoardCorners; i++)
         {
-            SDL_Vertex vertex_1 = {{WIDTH / 2.0, HEIGHT / 2.0}, {50, 50, 50, 0}, {1, 1}};
+            SDL_Vertex vertex_1 = {{(float)(WIDTH / 2.0), (float)(HEIGHT / 2.0)}, {50, 50, 50, 0}, {1, 1}};
             SDL_Vertex vertex_2 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * 5), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * 5)}, {50, 50, 50, 255}, {1, 1}};
             SDL_Vertex vertex_3 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * 5), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * 5)}, {50, 50, 50, 255}, {1, 1}};
 
-            // Put them into array
             SDL_Vertex vertices[] = {
                 vertex_1,
                 vertex_2,
