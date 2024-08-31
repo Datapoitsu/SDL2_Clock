623c46e9506b7007046a95638073a04b661a9396
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-08-31 23:42:21 +0300

    Fixed resizing of window

--- main.cpp
+++ main.cpp
@@ -4,12 +4,47 @@
 #include <ctime>
 #define PI 3.141592654
 
-int WIDTH = 400, HEIGHT = 400;
+// -- Preference variables -- //
+int width = 400, height = 400;
+float hourArrowLength = 0.55;
+float minuteArrowLength = 0.75;
+float secondArrowLength = 0.80;
+float markStartLenght = 0.90;
+float markEndLenght = 0.95;
+float markFiveStartLenght = 0.85;
+float markFiveEndLenght = 0.95;
+float boardSize = 1.0f;
+float emptySize = 0.1f;
+float knobSize = 0.025f;
+SDL_Color backGroundColor = {125,125,125,255};
+SDL_Color boardColor = {255,255,255,255};
+SDL_Color hourArrowColor = {0,0,0,255};
+SDL_Color minuteArrowColor = {0,0,0,255};
+SDL_Color secondArrowColor = {255,0,0,255};
+SDL_Color markerColor = {0,0,0,255};
+SDL_Color markerFiveColor = {0,0,0,255};
+SDL_Color knobColor = {50,50,50,255};
+const int ClockBoardCorners = 32;
 
-int main( int argc, char *argv[])
+// -- Don't touch variables -- //
+int sizeMultiplier = 0;
+int LastSecond = 0;
+
+int SetClockSize(SDL_Window *window)
+{
+    int w, h, size;
+    SDL_GetWindowSize(window,&w,&h);
+    size = ((float)h - (float)h * emptySize) / 2;
+    if(w < h){
+        size = ((float)w - (float)w * emptySize) / 2;
+    }
+    return size;
+}
+
+int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("SDL Clock", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("SDL Clock", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
     if (window == NULL)
     {
@@ -17,17 +52,9 @@ int main( int argc, char *argv[])
         return 1;
     }
 
-    //Clocksize used for dynamic sizing.
-    int ClockSize = (HEIGHT - 100) / 2;
-    if(WIDTH < HEIGHT){
-        ClockSize = (WIDTH - 100) / 2;
-    }
+    sizeMultiplier = SetClockSize(window);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-    
-    const int ClockBoardCorners = 32;
-    int LastSecond = 0;
-
     SDL_Event windowEvent;
 
     while (true)
@@ -39,6 +66,10 @@ int main( int argc, char *argv[])
                 break;
             }
         }
+        if(windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED){
+            sizeMultiplier = SetClockSize(window);
+            SDL_GetWindowSize(window,&width,&height);
+        }
         
         // -- Calculating the time -- //
         const time_t now = time(nullptr);
@@ -46,34 +77,26 @@ int main( int argc, char *argv[])
         float Seconds = (float)calendar_time.tm_sec;
         
         //Skips rendering if data doesn't change.
-        if(Seconds == LastSecond){
+        if(Seconds == LastSecond)
+        {
             continue;
         }
-        else{
-            LastSecond = Seconds;
-        }
+        LastSecond = Seconds;
 
         float Minutes = (float)calendar_time.tm_min + Seconds / 60.0; // Adds division of the lower unit, so arrays work like in a real analog clock
         float Hours = fmod(((float)calendar_time.tm_hour + Minutes / 60.0), 12.0);
 
         // Background Color
-        SDL_SetRenderDrawColor(renderer, 125, 125, 125, 255);
-        //SDL_RenderPresent(renderer);
+        SDL_SetRenderDrawColor(renderer, backGroundColor.r, backGroundColor.g, backGroundColor.b, backGroundColor.a);
         SDL_RenderClear(renderer);
 
         // -- Clock board -- //
         for (int i = 0; i < ClockBoardCorners; i++)
         {
-            SDL_Vertex vertex_1 = {{(float)(WIDTH / 2.0), (float)(HEIGHT / 2.0)}, {255, 255, 255, 255}, {1, 1}};
-            SDL_Vertex vertex_2 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * 1.05 * ClockSize), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * 1.05 * ClockSize)}, {255, 255, 255, 255}, {1, 1}};
-            SDL_Vertex vertex_3 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * 1.05 * ClockSize), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * 1.05 * ClockSize)}, {255, 255, 255, 255}, {1, 1}};
-
-            SDL_Vertex vertices[] = {
-                vertex_1,
-                vertex_2,
-                vertex_3
-            };
-
+            SDL_Vertex vertex_1 = {{(float)(width / 2.0), (float)(height / 2.0)}, boardColor, {1, 1}}; //Center vertex
+            SDL_Vertex vertex_2 = {{(float)(width / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * boardSize * sizeMultiplier), (float)(height / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * boardSize * sizeMultiplier)}, boardColor, {1, 1}};
+            SDL_Vertex vertex_3 = {{(float)(width / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * boardSize * sizeMultiplier), (float)(height / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * boardSize * sizeMultiplier)}, boardColor, {1, 1}};
+            SDL_Vertex vertices[] = {vertex_1,vertex_2,vertex_3};
             SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
         }
 
@@ -81,33 +104,38 @@ int main( int argc, char *argv[])
         for (int i = 0; i < 60; i ++)
         {
             float angle = fmod(PI * 2.0 / 60 * i, PI * 2.0);
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-            if(i % 5 == 0)
+            if(i % 5 != 0) //Not five minute marker
             {
-                SDL_RenderDrawLine(renderer, WIDTH / 2.0 + cos(angle) * 0.92 * ClockSize, HEIGHT / 2.0 + sin(angle) * 0.92 * ClockSize, WIDTH / 2.0 + cos(angle) * ClockSize, HEIGHT / 2.0 + sin(angle) * ClockSize);
+                SDL_RenderDrawLine(renderer, width / 2.0 + cos(angle) * markStartLenght * sizeMultiplier, height / 2.0 + sin(angle) * markStartLenght * sizeMultiplier, width / 2.0 + cos(angle) * markEndLenght * sizeMultiplier, height / 2.0 + sin(angle) * markEndLenght * sizeMultiplier);
             }
-            SDL_RenderDrawLine(renderer, WIDTH / 2.0 + cos(angle) * 0.95 * ClockSize, HEIGHT / 2.0 + sin(angle) * 0.95 * ClockSize, WIDTH / 2.0 + cos(angle) * ClockSize, HEIGHT / 2.0 + sin(angle) * ClockSize);
+            else //Five minute marker
+            {
+                SDL_SetRenderDrawColor(renderer, markerColor.r, markerColor.g, markerColor.b, markerColor.a);
+                SDL_RenderDrawLine(renderer, width / 2.0 + cos(angle) * markFiveStartLenght * sizeMultiplier, height / 2.0 + sin(angle) * markFiveStartLenght * sizeMultiplier, width / 2.0 + cos(angle) * markFiveEndLenght * sizeMultiplier, height / 2.0 + sin(angle) * markFiveEndLenght * sizeMultiplier);
+                SDL_SetRenderDrawColor(renderer, markerFiveColor.r, markerFiveColor.g, markerFiveColor.b, markerFiveColor.a); //Resetting the color
+            }
         }
 
         // -- Arrows -- //
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.0 + cos(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * 0.6 * ClockSize, HEIGHT / 2.0 + sin(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * 0.6 * ClockSize);
 
-        SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.0 + cos(fmod((Minutes + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.80 * ClockSize, HEIGHT / 2.0 + sin(fmod((Minutes + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.80 * ClockSize);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 2, WIDTH / 2.0 + cos(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.90 * ClockSize, HEIGHT / 2.0 + sin(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * 0.90 * ClockSize);
+        //hour
+        SDL_SetRenderDrawColor(renderer, hourArrowColor.r, hourArrowColor.g, hourArrowColor.b, hourArrowColor.a);
+        SDL_RenderDrawLine(renderer, width / 2, height / 2, width / 2.0 + cos(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * hourArrowLength * sizeMultiplier, height / 2.0 + sin(fmod((Hours + 9.0) / 12.0 * PI * 2.0, PI * 2.0)) * hourArrowLength * sizeMultiplier);
+        //minute
+        SDL_SetRenderDrawColor(renderer, minuteArrowColor.r, minuteArrowColor.g, minuteArrowColor.b, minuteArrowColor.a);
+        SDL_RenderDrawLine(renderer, width / 2, height / 2, width / 2.0 + cos(fmod((Minutes + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * minuteArrowLength * sizeMultiplier, height / 2.0 + sin(fmod((Minutes + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * minuteArrowLength * sizeMultiplier);
+        //second
+        SDL_SetRenderDrawColor(renderer, secondArrowColor.r, secondArrowColor.g, secondArrowColor.b, secondArrowColor.a);
+        SDL_RenderDrawLine(renderer, width / 2, height / 2, width / 2.0 + cos(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * secondArrowLength * sizeMultiplier, height / 2.0 + sin(fmod((Seconds + 45.0) / 60.0 * PI * 2.0, PI * 2.0)) * secondArrowLength * sizeMultiplier);
+        
+        // -- Knob -- //
         for (int i = 0; i < ClockBoardCorners; i++)
         {
-            SDL_Vertex vertex_1 = {{(float)(WIDTH / 2.0), (float)(HEIGHT / 2.0)}, {50, 50, 50, 0}, {1, 1}};
-            SDL_Vertex vertex_2 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * 5), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * 5)}, {50, 50, 50, 255}, {1, 1}};
-            SDL_Vertex vertex_3 = {{(float)(WIDTH / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * 5), (float)(HEIGHT / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * 5)}, {50, 50, 50, 255}, {1, 1}};
-
-            SDL_Vertex vertices[] = {
-                vertex_1,
-                vertex_2,
-                vertex_3};
-
+            SDL_Vertex vertex_1 = {{(float)(width / 2.0), (float)(height / 2.0)}, knobColor, {1, 1}};
+            SDL_Vertex vertex_2 = {{(float)(width / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * i)) * knobSize * sizeMultiplier), (float)(height / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * i)) * knobSize * sizeMultiplier)}, knobColor, {1, 1}};
+            SDL_Vertex vertex_3 = {{(float)(width / 2.0 + (cos(PI * 2.0 / ClockBoardCorners * (i + 1))) * knobSize * sizeMultiplier), (float)(height / 2.0 + (sin(PI * 2.0 / ClockBoardCorners * (i + 1))) * knobSize * sizeMultiplier)}, knobColor, {1, 1}};
+            SDL_Vertex vertices[] = {vertex_1,vertex_2,vertex_3};
             SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
         }
 
